be9db31d05f97749ac14dd468e510421ee51c547
Jan Idziak <Jan.Idziak@gmail.com>
2016-05-31 00:33:39 +0200

    Get i-th document of new corpus
    Added new test for testin getter of new corpus constructed based on many
    (100) articles.

--- tests/testthat/testCorpus.R
+++ tests/testthat/testCorpus.R
@@ -11,3 +11,8 @@ test_that("Simple vector constructor", {
   rd <- new_corpus(c("doc_1", "doc_2"))
   expect_equal(getDoc(rd, 2), "doc_2")
 })
+
+test_that("Complex vector constructor", {
+  rd <- new_corpus(paste("doc_", 1:100, sep = ""))
+  expect_equal(getDoc(rd, 14), "doc_14")
+})
