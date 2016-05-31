cf90c2dd1c1cba3644c36b92e97208f0e0351145
Jan Idziak <Jan.Idziak@gmail.com>
2016-05-31 18:28:31 +0200

    Parse two simple articles

--- R/corpus.R
+++ R/corpus.R
@@ -24,7 +24,7 @@ new_corpus <- function(x = NULL) {
 getDoc <- function(x, i) {
   if (length(x$text) < i)
     stop("index \"i\" out of bands")
-  x$text[i]
+  x$text[[i]]
 }
 
 #' Function to access metadata for documents from new corpus
@@ -43,4 +43,33 @@ getMeta <- function(x, i, parameter) {
   if (length(meta_vector) < i)
     stop("index \"i\" out of bands")
   meta_vector[i]
+}
+
+#' Function to create new parsed corpus
+#'
+#'
+#' @param x source
+#' @return returns new corpus list
+#'
+#' @export
+new_parsed <- function(x = NULL) {
+  if (is.null(x)) {
+    stop("argument \"x\" is missing")
+  }
+  x <- structure(list(text = x, language = rep("en", length(x))),
+                 class = "new_parsed")
+  x
+}
+
+#' Function to parse new corpus
+#'
+#' @param x corpus
+#' @return returns new parsed list
+#'
+#' @export
+parse <- function(x) {
+  parsed_doc_list <- sapply(x$text, function(y) strsplit(y, " "))
+  names(parsed_doc_list) <- NULL
+  x <- new_parsed(parsed_doc_list)
+  x
 }
