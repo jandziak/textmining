7098c981ad33141f873d2fba10dbebc47cca0822
Jan Idziak <Jan.Idziak@gmail.com>
2016-07-05 11:45:24 +0200

    Reading from directory for simple read tests deleted

--- tests/testthat/testExterior.R
+++ tests/testthat/testExterior.R
@@ -1,24 +1,6 @@
 # context("Exterior packages corpus read")
 # test_that("Reading corpus from other files works", {
 #   dir.create("tmp")
-#   write.table("This is written file", "tmp/tmp1.txt")
-#   rd <- try(tmCorpus(source = "tmp"))
-#   unlink("tmp", recursive = TRUE)
-#   expect_equal(rd, tmCorpus("This is written file"))
-# })
-#
-# test_that("Reading corpus from other files works", {
-#   dir.create("tmp")
-#   write.table("This is written file", "tmp/tmp1.txt")
-#   write.table("This is written file 2", "tmp/tmp2.txt")
-#   rd <- try(tmCorpus(source = "tmp"))
-#   unlink("tmp", recursive = TRUE)
-#   expect_equal(rd, tmCorpus(c("This is written file",
-#                               "This is written file 2")))
-# })
-#
-# test_that("Reading corpus from other files works", {
-#   dir.create("tmp")
 #   writeLines("This is written file", "tmp/tmp1.txt")
 #   writeLines("This is written file 2", "tmp/tmp2.txt")
 #   rd <- try(tmCorpus(source = "tmp", method = "stylo"))
